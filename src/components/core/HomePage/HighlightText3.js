e08ea011ff78b6c5e5b4e29f99a95aa8d1545e1e
22-Anshika <168359867+22-Anshika@users.noreply.github.com>
2025-10-12 13:51:37 +0530

    Initial commit

---
+++ src/components/core/HomePage/HighlightText3.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HighlightText3 = ({text}) => {
+  return (
+    <span className='bg-gradient-to-b from-[#4a5884] to-[#909eb2] 
+    text-transparent bg-clip-text font-bold'>
+        {" "}
+        {text}
+        
+    </span>
+  )
+}
+
+export default HighlightText3
---
+++ src/components/core/HomePage/HiglightText2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HiglightText2 = ({text}) => {
+  return (
+    <span className='bg-gradient-to-b from-[#6735c4] to-[#c885d6] 
+    text-transparent bg-clip-text font-bold'>
+        {" "}
+        {text}
+        
+    </span>
+  )
+}
+
+export default HiglightText2
